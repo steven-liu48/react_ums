89b230e197574e3d22ed3440fc817766931843a2
Steven Liu <xiaoxiang.liu48@gmail.com>
2020-11-15 20:42:20 +0800

    Made changes

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -3,8 +3,14 @@ import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <div className="NotFound text-center">
-      <h3>404 Page not found</h3>
-    </div>
+    // <div className="NotFound text-center">
+    //   <h3>404 Page not found</h3>
+    // </div>
+        <div className="Home">
+        <div className="lander">
+          <h1>Hi there!</h1>
+          <p className="text-muted">Welcome.</p>
+        </div>
+      </div>
   );
 }
