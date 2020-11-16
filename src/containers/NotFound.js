1e2c31c169b3d46886139f893be1c8cca068cd17
Steven Liu <xiaoxiang.liu48@gmail.com>
2020-11-16 16:55:51 +0800

    Made changes

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -3,16 +3,15 @@ import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    // <div className="NotFound text-center">
-    //   <h3>404 Page not found</h3>
-    // </div>
-        <div className="Home">
-        <div className="lander">
-          <h1>Welcome!</h1>
-          <p className="text-muted">This is a React App with a user login system powered by AWS Cognito.<br></br>
-          <a href="https://github.com/steven-liu48/react_ums" target="_blank">See the source code the GitHub❭</a>
-          </p>
+    <div className="Home">
+      <div className="lander">
+        <h1>Welcome!</h1>
+        <p className="text-muted">This is a React App with a user login system powered by AWS Cognito.<br></br>
+        <a href="https://github.com/steven-liu48/react_ums" target="_blank">Check the Source Code ></a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://steven-liu48.github.io/home-page/" target="_blank">About the Author ></a>
+        </p>
       </div>
-      </div>
+    </div>
   );
 }
